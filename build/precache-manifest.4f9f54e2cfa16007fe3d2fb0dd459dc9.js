8a1d195e3ea025901f34bb133d092eae0719f6bc
MELVIN GEORGE <melvingeorge10@gmail.com>
2019-11-24 21:18:24 +0530

    minor fix

---
+++ build/precache-manifest.4f9f54e2cfa16007fe3d2fb0dd459dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d022ad4634ead9eae79e31dcc0dc23c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "334b1c9c22736e48a317",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "aadb5854537db2af8990",
+    "url": "/static/css/main.5229bee3.chunk.css"
+  },
+  {
+    "revision": "334b1c9c22736e48a317",
+    "url": "/static/js/2.ec3994f2.chunk.js"
+  },
+  {
+    "revision": "aadb5854537db2af8990",
+    "url": "/static/js/main.ae7bd80e.chunk.js"
+  },
+  {
+    "revision": "445f160729df22e89868",
+    "url": "/static/js/runtime-main.0181421a.js"
+  },
+  {
+    "revision": "61fbdb11aa0079cd1cbf6bb59c709c35",
+    "url": "/static/media/img1.61fbdb11.png"
+  }
+]);
