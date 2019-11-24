3d9e728428e1edc224167f398bbaa217a5443cbe
MELVIN GEORGE <melvingeorge10@gmail.com>
2019-11-25 03:15:29 +0530

    added compression

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const compression = require('compression');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 
+// compress all responses
+app.use(compression());
+
 app.use('/',express.static(path.join(__dirname,'/total-cloud-intern/build')));
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Server Running on PORT : ${PORT}`)
