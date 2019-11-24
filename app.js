aa4ec8c8d2e1e9b6feeacc050fc476bb0385ea6e
MELVIN GEORGE <melvingeorge10@gmail.com>
2019-11-24 21:16:00 +0530

    minor fix

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = 8080 || process.env.PORT;
 
-app.use(express.static(path.join(__dirname,'/total-cloud-intern/build')));
-
-
-
+app.use('/',express.static(path.join(__dirname,'/total-cloud-intern/build')));
 
 app.listen(PORT, ()=>{
     console.log(`Server Running on PORT : ${PORT}`)
